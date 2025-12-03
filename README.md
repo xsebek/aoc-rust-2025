99c59a13ef5f5384d85355e0c93ea93263a415ef
Ondřej Weis <ondras98@icloud.com>
2025-12-03 11:31:02 +0100

    Solve day 2 part 2

--- README.md
+++ README.md
@@ -18,9 +18,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `83.1µs` | `80.4µs` |
+| [Day 1](./src/bin/01.rs) | `81.7µs` | `79.7µs` |
+| [Day 2](./src/bin/02.rs) | `12.8µs` | `41.8µs` |
 
-**Total: 0.16ms**
+**Total: 0.22ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 
 advent_of_code::solution!(2);
 
-pub fn part_one(input: &str) -> Option<i64> {
+pub fn part_one(input: &str) -> Option<i128> {
     let ranges = parse(input);
     Some(ranges.into_iter().map(count_silly).sum())
 }
@@ -15,8 +15,8 @@ fn parse(input: &str) -> Vec<Range> {
         .map(|r| {
             let (f, l) = r.trim().split_once('-').unwrap();
             Range {
-                first: i64::from_str(f).unwrap(),
-                last: i64::from_str(l).unwrap(),
+                first: i128::from_str(f).unwrap(),
+                last: i128::from_str(l).unwrap(),
             }
         })
         .collect_vec()
@@ -24,48 +24,79 @@ fn parse(input: &str) -> Vec<Range> {
 
 #[derive(Debug, Copy, Clone)]
 struct Range {
-    first: i64,
-    last: i64,
+    first: i128,
+    last: i128,
 }
 
 impl Range {
-    fn contains(self, value: i64) -> bool {
+    fn contains(self, value: i128) -> bool {
         self.first <= value && value <= self.last
     }
 }
 
-fn count_silly(range: Range) -> i64 {
-    let mut result = 0;
-    let mut count = 0;
+fn count_silly(range: Range) -> i128 {
+    count_silly_generic(2, range).into_iter().sum()
+}
+
+fn repeat(digits: i128, times: u32) -> i128 {
+    let mut result = digits;
+    let exp = 10i128.pow(digits.ilog10() + 1);
+    for _ in 1..times {
+        result = result * exp + digits
+    }
+    result
+}
+
+pub fn part_two(input: &str) -> Option<i128> {
+    let ranges = parse(input);
+    Some(ranges.into_iter().map(count_more_silly).sum())
+}
+
+fn count_more_silly(range: Range) -> i128 {
+    let max = range.last.ilog10() + 1;
+    (2..=max)
+        .flat_map(|d| count_silly_generic(d, range))
+        .unique()
+        .sum()
+}
+
+macro_rules! debug_println {
+    ($($arg:tt)*) => (#[cfg(debug_assertions)] println!($($arg)*));
+}
+
+fn count_silly_generic(repeats: u32, range: Range) -> Vec<i128> {
     let first_digits = range.first.ilog10() + 1;
     let last_digits = range.last.ilog10() + 1;
     let mut ids = Vec::new();
     for ds in first_digits..=last_digits {
-        if ds % 2 == 0 {
-            let part_digits = ds / 2;
-            let mut p = range.first / 10i64.pow(part_digits);
-            let mut n = p * 10i64.pow(part_digits) + p;
+        if ds % repeats == 0 {
+            let part_digits = ds / repeats;
+            let mut p = if ds == first_digits {
+                take_first_digits(part_digits, range.first)
+            } else {
+                10i128.pow(part_digits - 1)
+            };
+            let mut n = repeat(p, repeats);
             while n <= range.last {
                 if range.contains(n) {
-                    result += n;
-                    count += 1;
                     ids.push(n);
                 }
                 p += 1;
-                n = p * 10i64.pow(p.ilog10() + 1) + p;
+                n = repeat(p, repeats);
             }
+            debug_println!(
+                "{ds}/{repeats}: {f}-{l} has {count} invalid IDs, {ids:?}",
+                count = ids.len(),
+                f = range.first,
+                l = range.last,
+            );
         }
     }
-    println!(
-        "{f}-{l} has {count} invalid IDs, {ids:?}",
-        f = range.first,
-        l = range.last
-    );
-    result
+    ids
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+fn take_first_digits(digits: u32, number: i128) -> i128 {
+    number / 10i128.pow(number.ilog10() + 1 - digits)
 }
 
 #[cfg(test)]
@@ -80,7 +111,36 @@ mod tests {
 
     #[test]
     fn test_part_two() {
+        // 95-115 now has two invalid IDs, 99 and 111.
+        assert_eq!(
+            count_silly_generic(
+                2,
+                Range {
+                    first: 95,
+                    last: 115
+                }
+            ),
+            vec![99]
+        );
+        assert_eq!(
+            count_silly_generic(
+                3,
+                Range {
+                    first: 95,
+                    last: 115
+                }
+            ),
+            vec![111]
+        );
+
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
+    }
+
+    #[test]
+    fn test_helpers() {
+        assert_eq!(take_first_digits(2, 21), 21);
+        assert_eq!(take_first_digits(2, 1234), 12);
+        assert_eq!(take_first_digits(1, 609), 6);
     }
 }
