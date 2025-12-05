e16fcb711c6f060e89d2fe7278d817ae929f6091
Ondřej Weis <ondras98@icloud.com>
2025-12-05 16:25:40 +0100

    Solve day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,9 +1,12 @@
 #![feature(ascii_char)]
 #![feature(ascii_char_variants)]
+use std::io::Write;
 extern crate core;
 
-use ascii::Char::{CommercialAt, LineFeed};
+use ascii::Char::{CommercialAt, FullStop, LineFeed, SmallX};
 use std::ascii;
+use std::collections::HashSet;
+use std::io::stdout;
 use itertools::Itertools;
 
 advent_of_code::solution!(4);
@@ -11,13 +14,14 @@ advent_of_code::solution!(4);
 pub fn part_one(input: &str) -> Option<usize> {
     let map = Map2D::new(input);
     print_accessible(&map);
-    Some(accessible_paper(&map))
+    Some(accessible_paper(&map).count())
 }
 
 struct Map2D<'a> {
     raw: &'a [ascii::Char],
     cols: usize,
     rows: usize,
+    removed: HashSet<(usize, usize)>
 }
 
 impl Map2D<'_> {
@@ -27,7 +31,7 @@ impl Map2D<'_> {
         let cols = line_pos.next().unwrap();
         debug_assert!(line_pos.all(|p| (p+1).is_multiple_of(cols+1)), "map must be rectangle");
         let rows = raw.iter().positions(|&c| c == LineFeed).count();
-        Map2D {raw, cols, rows}
+        Map2D {raw, cols, rows, removed: HashSet::new()}
     }
 
     fn get_i(&self, row: isize, col: isize) -> Option<ascii::Char> {
@@ -39,7 +43,11 @@ impl Map2D<'_> {
     }
 
     fn get(&self, row: usize, col: usize) -> Option<ascii::Char> {
-        self.raw.get(row * (self.cols + 1) + col).copied()
+        if self.removed.contains(&(row, col)) {
+            Some(FullStop)
+        } else {
+            self.raw.get(row * (self.cols + 1) + col).copied()
+        }
     }
 
     fn neighbors(&self, row: usize, col: usize) -> [Option<ascii::Char>; 8] {
@@ -63,28 +71,45 @@ fn is_paper_accessible(map: &Map2D, row: usize, col: usize) -> bool {
     papers_near < 4
 }
 
-fn accessible_paper(map: &Map2D) -> usize {
+fn accessible_paper(map: &Map2D) -> impl Iterator<Item=(usize, usize)> {
     (0..map.rows)
         .cartesian_product(0..map.cols)
         .filter(|&(r, c)| is_paper(map, r, c) && is_paper_accessible(map, r, c))
-        .count()
+}
+
+fn show_pos(map: &Map2D, row: usize, col: usize) -> ascii::Char {
+    if !is_paper(map, row, col) {FullStop}
+    else if is_paper_accessible(map, row, col) { SmallX }
+    else {CommercialAt}
 }
 
 fn print_accessible(map: &Map2D) {
     for r in 0..map.rows {
         for c in 0..map.cols {
-            print!("{}",
-                if !is_paper(map, r, c) {'.'}
-                else if is_paper_accessible(map, r, c) {'x'}
-                else {'@'}
-            )
+            print!("{}", show_pos(map, r, c))
         }
         println!()
     }
 }
 
-pub fn part_two(_: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    Some(take_all_accessible_paper(&mut Map2D::new(input)))
+}
+
+fn take_accessible_paper(map: &mut Map2D) -> bool {
+    let accessible = HashSet::from_iter(accessible_paper(map));
+    map.removed = map.removed.union(&accessible).copied().collect();
+    !accessible.is_empty()
+}
+
+fn take_all_accessible_paper(map: &mut Map2D) -> usize {
+    while take_accessible_paper(map) {
+        let mut lock = stdout().lock();
+        writeln!(lock).expect("no error");
+        print_accessible(map);
+        println!();
+    }
+    map.removed.len()
 }
 
 #[cfg(test)]
@@ -100,6 +125,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
