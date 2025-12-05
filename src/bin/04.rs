57e1cace66d134e41e9ac24b582a5e749b2700f6
Ondřej Weis <ondras98@icloud.com>
2025-12-05 17:07:23 +0100

    Refactor by moving map to lib

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -3,11 +3,11 @@
 use std::io::Write;
 extern crate core;
 
-use ascii::Char::{CommercialAt, FullStop, LineFeed, SmallX};
 use std::ascii;
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io::stdout;
-use itertools::Itertools;
+use std::iter::repeat;
+use advent_of_code::Map2D;
 
 advent_of_code::solution!(4);
 
@@ -17,70 +17,30 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(accessible_paper(&map).count())
 }
 
-struct Map2D<'a> {
-    raw: &'a [ascii::Char],
-    cols: usize,
-    rows: usize,
-    removed: HashSet<(usize, usize)>
-}
-
-impl Map2D<'_> {
-    fn new(input: &'_ str) -> Map2D<'_> {
-        let raw = input.as_ascii().unwrap();
-        let mut line_pos = raw.iter().positions(|&c| c == LineFeed);
-        let cols = line_pos.next().unwrap();
-        debug_assert!(line_pos.all(|p| (p+1).is_multiple_of(cols+1)), "map must be rectangle");
-        let rows = raw.iter().positions(|&c| c == LineFeed).count();
-        Map2D {raw, cols, rows, removed: HashSet::new()}
-    }
-
-    fn get_i(&self, row: isize, col: isize) -> Option<ascii::Char> {
-        if row < 0 || col < 0 {
-            None
-        } else {
-            self.get(row as usize, col as usize)
-        }
-    }
-
-    fn get(&self, row: usize, col: usize) -> Option<ascii::Char> {
-        if self.removed.contains(&(row, col)) {
-            Some(FullStop)
-        } else {
-            self.raw.get(row * (self.cols + 1) + col).copied()
-        }
-    }
-
-    fn neighbors(&self, row: usize, col: usize) -> [Option<ascii::Char>; 8] {
-        Itertools::collect_array((-1..=1)
-            .cartesian_product(-1..=1)
-            .filter(|&r| r != (0,0))
-            .map(|(r, c)| self.get_i(row as isize + r, col as isize + c)))
-            .expect("8 neighbors")
-    }
-}
+const PAPER: ascii::Char = ascii::Char::CommercialAt;
+const ACCESSIBLE: ascii::Char = ascii::Char::SmallX;
+const SPACE: ascii::Char = ascii::Char::FullStop;
 
 fn is_paper(map: &Map2D, row: usize, col: usize) -> bool {
-    map.get(row, col) == Some(CommercialAt)
+    map.get(row, col) == Some(PAPER)
 }
 
 fn is_paper_accessible(map: &Map2D, row: usize, col: usize) -> bool {
     let papers_near = map.neighbors(row, col)
         .into_iter()
-        .filter(|&c| c == Some(CommercialAt))
+        .filter(|&c| c == Some(PAPER))
         .count();
     papers_near < 4
 }
 
 fn accessible_paper(map: &Map2D) -> impl Iterator<Item=(usize, usize)> {
-    (0..map.rows)
-        .cartesian_product(0..map.cols)
-        .filter(|&(r, c)| is_paper(map, r, c) && is_paper_accessible(map, r, c))
+    map.range().filter(|&(r, c)| is_paper(map, r, c) && is_paper_accessible(map, r, c))
 }
 
 fn show_pos(map: &Map2D, row: usize, col: usize) -> ascii::Char {
-    if !is_paper(map, row, col) {FullStop}
-    else if is_paper_accessible(map, row, col) { SmallX }
-    else {CommercialAt}
+    if !is_paper(map, row, col) { SPACE }
+    else if is_paper_accessible(map, row, col) { ACCESSIBLE }
+    else { PAPER }
 }
 
 fn print_accessible(map: &Map2D) {
@@ -97,9 +57,9 @@ pub fn part_two(input: &str) -> Option<usize> {
 }
 
 fn take_accessible_paper(map: &mut Map2D) -> bool {
-    let accessible = HashSet::from_iter(accessible_paper(map));
-    map.removed = map.removed.union(&accessible).copied().collect();
-    !accessible.is_empty()
+    let count_before = map.overwrite_count();
+    map.set_many(HashMap::from_iter(accessible_paper(map).zip(repeat(SPACE))));
+    count_before != map.overwrite_count()
 }
 
 fn take_all_accessible_paper(map: &mut Map2D) -> usize {
@@ -109,7 +69,7 @@ fn take_all_accessible_paper(map: &mut Map2D) -> usize {
         print_accessible(map);
         println!();
     }
-    map.removed.len()
+    map.overwrite_count()
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,70 @@
+#![feature(ascii_char)]
+#![feature(ascii_char_variants)]
+
+use std::ascii;
+use std::ascii::Char::{FullStop, LineFeed};
+use std::collections::HashMap;
+use itertools::Itertools;
+
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+
+pub struct Map2D<'a> {
+    raw: &'a [ascii::Char],
+    pub cols: usize,
+    pub rows: usize,
+    overwrite: HashMap<(usize, usize), ascii::Char>
+}
+
+
+impl Map2D<'_> {
+    pub fn new(input: &'_ str) -> Map2D<'_> {
+        let raw = input.as_ascii().unwrap();
+        let mut line_pos = raw.iter().positions(|&c| c == LineFeed);
+        let cols = line_pos.next().unwrap();
+        debug_assert!(line_pos.all(|p| (p+1).is_multiple_of(cols+1)), "map must be rectangle");
+        let rows = raw.iter().positions(|&c| c == LineFeed).count();
+        Map2D {raw, cols, rows, overwrite: HashMap::new()}
+    }
+
+    pub fn range(&self) -> impl Iterator<Item=(usize, usize)> {
+        (0..self.rows).cartesian_product(0..self.cols)
+    }
+
+    pub fn get_i(&self, row: isize, col: isize) -> Option<ascii::Char> {
+        if row < 0 || col < 0 {
+            None
+        } else {
+            self.get(row as usize, col as usize)
+        }
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> Option<ascii::Char> {
+        self.overwrite
+            .get(&(row, col))
+            .or(self.raw
+                .get(row * (self.cols + 1) + col))
+            .copied()
+    }
+
+    pub fn neighbors(&self, row: usize, col: usize) -> [Option<ascii::Char>; 8] {
+        Itertools::collect_array((-1..=1)
+            .cartesian_product(-1..=1)
+            .filter(|&r| r != (0,0))
+            .map(|(r, c)| self.get_i(row as isize + r, col as isize + c)))
+            .expect("8 neighbors")
+    }
+
+    pub fn set(&mut self, row: usize, col: usize, value: ascii::Char) {
+        self.overwrite.insert((row, col), value);
+    }
+
+    pub fn set_many(&mut self, values: HashMap<(usize, usize), ascii::Char>) {
+        self.overwrite.extend(values)
+    }
+
+    pub fn overwrite_count(&self) -> usize {
+        self.overwrite.len()
+    }
+}
